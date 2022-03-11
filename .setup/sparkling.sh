3f637497ac706c397b9098014bec7afa55da91b5
Ethan Fann <ef@osano.com>
2022-03-10 17:38:47 -0800

    Add homebrew and nvm

--- .setup/sparkling.sh
+++ .setup/sparkling.sh
@@ -1,2 +1,11 @@
+# Xcode CLI Tools
 sudo softwareupdate -i -a
 xcode-select --install
+
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+
